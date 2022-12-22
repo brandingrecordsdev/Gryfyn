246a861171a615e715564ba894d83319f4cd7ef5
Adrian Chan <adrianchanuk@gmail.com>
2022-12-22 16:04:50 +0800

    fade in out

--- public/js/script.js
+++ public/js/script.js
@@ -185,19 +185,19 @@ const funnyScroll = new FunnyScroll({
             },            
             'enterEnd': (section) => {
                 section.classList.add('enter-end')   
-                document.querySelector('.nav-btn').style = 'opacity: 0;' 
-                document.querySelector('#landing-wallet-btn').style = 'opacity: 1;'
+                document.querySelector('.nav-btn').style = 'display:none;' 
+                document.querySelector('#landing-wallet-btn').style = 'display:block;'
                 
             },  
             'leaveEnd': (section) => {
                 section.classList.remove('enter-start')
                 section.classList.remove('enter-end')
-                document.querySelector('#landing-wallet-btn').style = 'opacity: 0;'
+                document.querySelector('#landing-wallet-btn').style = 'display: none;'
             },                               
         },        
         '1': {
             'enterStart': (section) => {
-                document.querySelector('.nav-btn').style = 'opacity: 1;'               
+                document.querySelector('.nav-btn').style = 'display: block;'               
             },  
             enterEnd: () => {
                 document.getElementById('star-divider').style = 'transform: rotate(0deg);'
